c75fdb3f8b89857fab8b216346d009b4151bee32
Huihual <lim_hwee_hua@tech.gov.sg>
2025-05-18 08:33:42 +0000

    updatetoindex

--- js/script.js
+++ js/script.js
@@ -4,13 +4,11 @@ document.getElementById('loginForm').addEventListener('submit', (e) => {
   const username = document.getElementById('username').value.trim();
 
   if (username !== "") {
-    // You can store the username if needed for later pages
     localStorage.setItem("username", username);
-
-    // Redirect to dashboard.html
     window.location.href = 'dashboard.html';
   } else {
     alert("Please enter a username.");
   }
 });
+
 
